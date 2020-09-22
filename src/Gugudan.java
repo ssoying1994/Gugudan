e28ee1a7f0b3766ac69c0956c93bd618ddf526d6
clove <clove@DESKTOP-M6SD93C>
2020-09-22 23:40:37 +0900

    gugudan

---
+++ src/Gugudan.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Gugudan {
+	public static void main(String[] args) {
+//
+		System.out.println("구구단 중 출력할 단은? :");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		System.out.println(number*1);
+		System.out.println(number*2);
+		System.out.println(number*3);
+		System.out.println(number*4);
+		System.out.println(number*5);
+		System.out.println(number*6);
+		System.out.println(number*7);
+		System.out.println(number*8);
+		System.out.println(number*9);
+		
+	}
+
+}
